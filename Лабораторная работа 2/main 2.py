36874b91d56521342ce35977dca18a26c7fd310b
Sasha02751 <sasha.kozlov.27062004@gmail.com>
2024-12-25 00:49:07 +0300

    Update main 2.py

--- Лабораторная работа 2/main 2.py
+++ Лабораторная работа 2/main 2.py
@@ -1,12 +1,18 @@
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
-mounth = 0  # Количество месяцев без долгов
-money_capital = money_capital - spend + salary
-while money_capital > 1000:
-    mounth +=1
-    spend = spend + spend * increase
-    money_capital = money_capital - spend + salary
-print("Количество месяцев, которое можно протянуть без долгов:", mounth)
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+def calculate_safety_cushion(salary, spend, months, increase):
+    money_capital = 0  # Начальное значение подушки безопасности
+    for _ in range(months):
+        # Покрытие зп
+        deficit = spend - salary
+        if deficit > 0:
+            # Покрытие подушкой
+            money_capital += deficit
+        # Увеличиваем расходы на следующий месяц
+        spend *= (1 + increase)
+    return round(money_capital)
+money_capital = calculate_safety_cushion(salary, spend, months, increase)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
